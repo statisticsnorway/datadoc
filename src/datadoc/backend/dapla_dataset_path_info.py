f2e46feebb3b256a82a3694abbeaca48acba9123
mmwinther <mmw@ssb.no>
2024-01-24 14:05:27 +0100

    First implementation of contains_data_from

--- src/datadoc/backend/dapla_dataset_path_info.py
+++ src/datadoc/backend/dapla_dataset_path_info.py
@@ -1,17 +1,47 @@
 """Extract info from an path following SSB`s naming convention."""
-import datetime
+from __future__ import annotations
+
+import contextlib
 import pathlib
+import re
+from typing import TYPE_CHECKING
+
+import arrow
+
+if TYPE_CHECKING:
+    import datetime
 
 
 class DaplaDatasetPathInfo:
     """Extract info from an path following SSB`s naming convention."""
 
+    year_regex = re.compile(r"p(19|20|21)\d{2}")
+
     def __init__(self, dataset_path: str) -> None:
         """Read info from an path following SSB`s naming convention."""
         self.dataset_path = pathlib.Path(dataset_path)
         self.dataset_name_sections = self.dataset_path.stem.split("_")
+        _period_strings = self._extract_period_strings(self.dataset_name_sections)
+        self.first_period_string = _period_strings[0]
+        self.second_period_string: str | None = None
+
+        with contextlib.suppress(IndexError):
+            self.second_period_string = _period_strings[1]
+
+    def _extract_period_strings(self, dataset_name_sections: list[str]) -> list[str]:
+        """Extract period strings from dataset name sections.
+
+        Iterates over the dataset name sections and returns a list of strings
+        that match the year regex, stripping the first character. This extracts
+        the year periods from the dataset name.
+        """
+        return [
+            x[1:]
+            for x in dataset_name_sections
+            if re.match(self.year_regex, x) is not None
+        ]
 
     @property
     def contains_data_from(self) -> datetime.date:
-        """Read dataset from date in ISO format year-month-day."""
-        return
+        """The earliest date from which data in the dataset is relevant for."""
+        return arrow.get(self.first_period_string).date()
--- tests/backend/test_dapla_dataset_path_info.py
+++ tests/backend/test_dapla_dataset_path_info.py
@@ -6,5 +6,4 @@ from datadoc.backend.dapla_dataset_path_info import DaplaDatasetPathInfo
 def test_exctract_period_info_date_from():
     file_name = "ufo_observasjoner_p2019_p2020_v1.parquet"
     dapla_dataset = DaplaDatasetPathInfo(file_name)
-    print(dapla_dataset.dataset_name_sections)
     assert dapla_dataset.contains_data_from == datetime.date(2019, 1, 1)
