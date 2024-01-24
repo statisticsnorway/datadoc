659e7993fe72b5292d1bc76ce0cd9514a63721ac
Jan Sander <63044278+JanhSander@users.noreply.github.com>
2024-01-24 13:35:03 +0100

    Started some work

--- src/datadoc/backend/dapla_dataset_path_info.py
+++ src/datadoc/backend/dapla_dataset_path_info.py
@@ -1,4 +1,6 @@
 """Extract info from an path following SSB`s naming convention."""
+import datetime
+import pathlib
 
 
 class DaplaDatasetPathInfo:
@@ -6,7 +8,10 @@ class DaplaDatasetPathInfo:
 
     def __init__(self, dataset_path: str) -> None:
         """Read info from an path following SSB`s naming convention."""
+        self.dataset_path = pathlib.Path(dataset_path)
+        self.dataset_name_sections = self.dataset_path.stem.split("_")
 
-    def contains_data_from(self) -> None:
+    @property
+    def contains_data_from(self) -> datetime.date:
         """Read dataset from date in ISO format year-month-day."""
         return
--- tests/backend/test_dapla_dataset_path_info.py
+++ tests/backend/test_dapla_dataset_path_info.py
@@ -6,4 +6,5 @@ from datadoc.backend.dapla_dataset_path_info import DaplaDatasetPathInfo
 def test_exctract_period_info_date_from():
     file_name = "ufo_observasjoner_p2019_p2020_v1.parquet"
     dapla_dataset = DaplaDatasetPathInfo(file_name)
-    assert dapla_dataset.contains_data_from() == datetime.date(2019, 1, 1)
+    print(dapla_dataset.dataset_name_sections)
+    assert dapla_dataset.contains_data_from == datetime.date(2019, 1, 1)
