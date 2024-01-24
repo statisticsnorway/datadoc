4d53ede5a13d109272c0c5e3cb21d91b6365e4ad
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-01-24 11:18:26 +0100

    Extract time period from dapla dataset path - set up class, method and test for extracting date from

---
+++ src/datadoc/backend/dapla_dataset_path_info.py
@@ -0,0 +1,12 @@
+"""Extract info from an path following SSB`s naming convention."""
+
+
+class DaplaDatasetPathInfo:
+    """Extract info from an path following SSB`s naming convention."""
+
+    def __init__(self, dataset_path: str) -> None:
+        """Read info from an path following SSB`s naming convention."""
+
+    def contains_data_from(self) -> None:
+        """Read dataset from date in ISO format year-month-day."""
+        return
