0bd3ed6519675542d147d69788a2aa344dcce3bc
Jorgen-5 <jrussnes@gmail.com>
2024-02-16 10:18:35 +0100

    Added error handling for get request to statistic subject structure xml file (#168)
    * Added error handling for source url in statistic subject mapping
    
    * Fixed type annoation and moved None check to check if statistic subhject structure is loaded before parsing it
    
    * Added tests for different http exceptions
    
    * Added requests-mock to noxfile and changed conftest subject mapping instatiation
    
    * Added requests_mock to noxfile
    
    ---------
    
    Co-authored-by: rlj <rlj@W51773.ssb.no>
    Co-authored-by: Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>

--- tests/backend/test_statistic_subject_mapping.py
+++ tests/backend/test_statistic_subject_mapping.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 from bs4 import BeautifulSoup
 
 from datadoc.backend.statistic_subject_mapping import PrimarySubject
@@ -46,11 +47,11 @@ STATISTICAL_SUBJECT_STRUCTURE_DIR = "statistical_subject_structure"
 )
 @pytest.mark.usefixtures("_mock_fetch_statistical_structure")
 def test_read_in_statistical_structure(
-    subject_mapping: StatisticSubjectMapping,
+    subject_mapping_fake_statistical_structure: StatisticSubjectMapping,
     expected: list[PrimarySubject],
 ) -> None:
-    subject_mapping.wait_for_primary_subject()
-    assert subject_mapping.primary_subjects == expected
+    subject_mapping_fake_statistical_structure.wait_for_primary_subject()
+    assert subject_mapping_fake_statistical_structure.primary_subjects == expected
 
 
 @pytest.mark.parametrize(
@@ -66,12 +67,30 @@ def test_read_in_statistical_structure(
 )
 @pytest.mark.usefixtures("_mock_fetch_statistical_structure")
 def test_get_secondary_subject(
-    subject_mapping: StatisticSubjectMapping,
+    subject_mapping_fake_statistical_structure: StatisticSubjectMapping,
     statistic_short_name: str,
     expected_secondary_subject: str,
 ) -> None:
-    subject_mapping.wait_for_primary_subject()
+    subject_mapping_fake_statistical_structure.wait_for_primary_subject()
     assert (
-        subject_mapping.get_secondary_subject(statistic_short_name)
+        subject_mapping_fake_statistical_structure.get_secondary_subject(
+            statistic_short_name,
+        )
         == expected_secondary_subject
     )
+
+
+@pytest.mark.parametrize(
+    ("exception_to_raise"),
+    [
+        (requests.exceptions.ConnectTimeout),
+        (requests.exceptions.HTTPError),
+        (requests.exceptions.ReadTimeout),
+        (requests.exceptions.ConnectionError),
+    ],
+)
+def test_subject_mapping_http_exception(
+    subject_mapping_http_exception: StatisticSubjectMapping,
+) -> None:
+    subject_mapping_http_exception.wait_for_primary_subject()
+    assert subject_mapping_http_exception.primary_subjects == []
