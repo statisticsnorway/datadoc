83652ab72145a6e3e2d3f9cbbc3bea7aaea0daf2
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-02-26 10:33:47 +0100

    Failing test to reproduce bug

--- tests/frontend/callbacks/test_dataset_callbacks.py
+++ tests/frontend/callbacks/test_dataset_callbacks.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 from typing import cast
 from unittest.mock import Mock
@@ -68,6 +69,45 @@ def test_accept_dataset_metadata_input_incorrect_data_type(metadata: DataDocMeta
     output = accept_dataset_metadata_input(3.1415, "dataset_state")
     assert output[0] is True
     assert "validation error for Dataset" in output[1]
+
+
+earlier = datetime.date(2020, 1, 1)
+later = datetime.date(2024, 1, 1)
+
+
+@pytest.mark.parametrize(
+    ("start_date", "end_date", "expect_error"),
+    [
+        (None, None, False),
+        (later, None, False),
+        (None, earlier, False),
+        (earlier, later, False),
+        (later, earlier, True),
+    ],
+    ids=[
+        "no-input",
+        "start-date-only",
+        "end-date-only",
+        "both-dates",
+        "incorrect-order",
+    ],
+)
+def test_accept_dataset_metadata_input_data_ordering(
+    metadata: DataDocMetadata,
+    start_date: datetime.date,
+    end_date: datetime.date,
+    expect_error: bool,  # noqa: FBT001
+):
+    state.metadata = metadata
+    accept_dataset_metadata_input(
+        str(start_date),
+        DatasetIdentifiers.CONTAINS_DATA_FROM.value,
+    )
+    output = accept_dataset_metadata_input(
+        str(end_date),
+        DatasetIdentifiers.CONTAINS_DATA_UNTIL.value,
+    )
+    assert output[0] is expect_error
 
 
 def test_update_dataset_metadata_language_strings(
