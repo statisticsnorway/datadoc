9a95b611f0fdf7abe6f5fc85e554026049d4fd87
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-03-04 10:59:27 +0100

    Test planning

--- tests/frontend/callbacks/test_callbacks_new_variables.py
+++ tests/frontend/callbacks/test_callbacks_new_variables.py
@@ -1,1 +1,9 @@
 """Tests for new variable design."""
+
+# test language ?
+# test dropdown ?
+
+# test get_variables_in_dataset and get_variable_short_names
+
+# callback_populate_new_variables_workspace -
+# callback_populate_edit_section
---
+++ tests/frontend/components/test_new_variables_tab.py
@@ -0,0 +1,4 @@
+"""Test new variables layout."""
+
+# test dbc.Form
+# test return in build_edit_section and ssb.Accordion
