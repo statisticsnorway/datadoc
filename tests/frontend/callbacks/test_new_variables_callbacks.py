7ae8d05048971eac794827f12208e49bd7bc4457
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-03-06 14:50:08 +0100

    Add testdata

--- tests/frontend/callbacks/test_new_variables_callbacks.py
+++ tests/frontend/callbacks/test_new_variables_callbacks.py
@@ -2,12 +2,13 @@
 
 import pytest
 
+from datadoc import state
+from datadoc.enums import SupportedLanguages
+from datadoc.frontend.callbacks.new_variables import build_new_variables_workspace
 from datadoc.frontend.components.resources_test_new_variables import build_edit_section
-
-# Import the names of callback functions you want to test
 from datadoc.frontend.components.resources_test_new_variables import build_ssb_accordion
-from datadoc.frontend.fields.display_new_variables import OPTIONAL_VARIABLES_METADATA
 
+# testdata/stubs/fixture - mocks?
 obligatory_header = "Obligatorisk"
 reccomended_header = "Anbefalt"
 accordion_key = {"type": "variables-accordion", "id": "pers_id"}
@@ -19,6 +20,34 @@ NORSK_NYNORSK = "nn"
 ENGLISH = "en"
 
 variable_short_names = ["pers_id", "tidspunkt", "sykepenger"]
+
+stub_variables_metadata = []
+
+# OBLIGATORY_VARIABLES_METADATA,
+# "Obligatorisk",
+# variable,
+# state.current_metadata_language.value
+
+# mock/stub state
+
+# state language callback_input = ENGLISH  state.current_metadata_language.value
+# state state.metadata.variables_lookup.keys()
+
+#   header: str,
+#   key: dict,
+#    variable_short_name: str,
+#  children list
+# stub build_ssb_accordion?
+
+DISPLAY_STUB_VARIABLES = {
+    "pers_id": {
+        "identifier": "short_name",
+        "display_name": "Kortnavn",
+        "description": "Fysisk navn på variabelen i datasettet. Bør tilsvare anbefalt kortnavn.",
+        "obligatory": True,
+        "editable": False,
+    },
+}
 
 
 # class build variable
@@ -39,6 +68,19 @@ def build_variables_list(build_variable):
 
 
 @pytest.fixture()
+def build_test_variables():
+    return [
+        (
+            {
+                "variable_short_name": i,
+                "id": {"type": "variables-accordion", "id": i},
+            }
+        )
+        for i in variable_short_names
+    ]
+
+
+@pytest.fixture()
 def variable_accordion(build_variable):
     return build_ssb_accordion(
         obligatory_header,
@@ -49,7 +91,13 @@ def variable_accordion(build_variable):
 
 
 def test_build_new_variables_workspace():
-    return None
+    state.current_metadata_language = SupportedLanguages.NORSK_BOKMÅL
+    state.metadata.variables_lookup = DISPLAY_STUB_VARIABLES
+    result_list = build_new_variables_workspace()
+    assert result_list is not None
+
+
+# self.variables_lookup dict[str, model.Variable] = {}
 
 
 def test_callback_populate_accordion_workspace(build_variable):
@@ -59,7 +107,7 @@ def test_callback_populate_accordion_workspace(build_variable):
         build_variable["id"],
         build_variable["variable_short_name"],
         children=build_edit_section(
-            OPTIONAL_VARIABLES_METADATA,
+            stub_variables_metadata,
             "Anbefalt",
             build_variable["variable_short_name"],
             callback_input,
@@ -68,18 +116,4 @@ def test_callback_populate_accordion_workspace(build_variable):
     assert output is not None
 
 
-"""def test_run_callback_populate_accordion_workspace():
-    def run_callback():
-        context_value.set(
-            AttributeDict(triggered_inputs=[{"prop_id": "language-dropdown"}]),
-        )
-        # return
-
-    ctx = copy_context()
-    output = ctx.run(run_callback)
-    assert output == ""
-
-"""
-# accept variable_metadata_input
-# callback populate_accordion_workspace(ENGLISH)
 # callback_accept_variable_metadata_input
