f5ba5c64c99559cee99d27faaea986842f3a0fba
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-03-08 11:14:03 +0100

    Tests new variables component ok

--- tests/frontend/components/test_new_variables_tab.py
+++ tests/frontend/components/test_new_variables_tab.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 import pytest
 import ssb_dash_components as ssb  # type: ignore[import-untyped]
 from dash import html
+from datadoc_model import model
 
 from datadoc.frontend.components.resources_test_new_variables import build_edit_section
 from datadoc.frontend.components.resources_test_new_variables import (
@@ -14,37 +15,14 @@ from datadoc.frontend.fields.display_new_variables import OBLIGATORY_VARIABLES_M
 from datadoc.frontend.fields.display_new_variables import OPTIONAL_VARIABLES_METADATA
 
 # set-up
-obligatory_header = "Obligatorisk"
-reccomended_header = "Anbefalt"
-
-edit_sections = []
 empty_metadata_input = []
 obligatory_metadata_input = OBLIGATORY_VARIABLES_METADATA
 optional_metadata_input = OPTIONAL_VARIABLES_METADATA
-# DISPLAYED_VARIABLES_METADATA
 
 NORSK_BOKMÅL = "nb"
 NORSK_NYNORSK = "nn"
 ENGLISH = "en"
 
-variable_short_names = ["pers_id", "tidspunkt", "sykepenger"]
-
-
-@pytest.fixture()
-def build_test_variables():
-    return [
-        (
-            {
-                "header": i,
-                "edit_section_id": i,
-                "id": {"type": "variables-accordion", "id": i},
-            }
-        )
-        for i in variable_short_names
-    ]
-
-
-# variable: model.Variable,
 ACCORDION_TYPE = "variables-accordion"
 ALERTS_TYPE = "variable-input-alerts"
 INPUT_TYPE = "variable-inputs"
@@ -185,6 +163,25 @@ ACCORDION_CHILDREN_ID = [
     ),
 ]
 
+INPUT_FIELDS = [
+    (
+        OBLIGATORY_VARIABLES_METADATA,
+        model.Variable(short_name="hoveddiagnose"),
+        NORSK_NYNORSK,
+    ),
+    (OPTIONAL_VARIABLES_METADATA, model.Variable(short_name="pers_id"), NORSK_BOKMÅL),
+    (
+        OBLIGATORY_VARIABLES_METADATA,
+        model.Variable(short_name="ber_bruttoformue"),
+        ENGLISH,
+    ),
+    (
+        OPTIONAL_VARIABLES_METADATA,
+        model.Variable(short_name="sykepenger"),
+        NORSK_BOKMÅL,
+    ),
+]
+
 
 @pytest.mark.parametrize(
     ("header", "key", "variable_short_name", "children", "expected"),
@@ -275,52 +272,40 @@ def test_build_ssb_accordion_children_return_correct_id(
     ]
 
 
-def test_build_edit_section():
-    current_variable = variable_short_names[0]
+def test_build_edit_section_empty_inputs():
     obligatory_edit_section = build_edit_section(
         empty_metadata_input,
-        obligatory_header,
-        current_variable,
-        NORSK_BOKMÅL,
+        "",
+        "",
+        "",
     )
-    assert obligatory_edit_section is not None
-
-
-def test_build_edit_section_children():
-    current_variable = variable_short_names[0]
-    obligatory_edit_section = build_edit_section(
-        empty_metadata_input,
-        obligatory_header,
-        current_variable,
-        NORSK_BOKMÅL,
-    )
-    assert obligatory_edit_section.children[0].children == "Obligatorisk"
+    assert isinstance(obligatory_edit_section, html.Section)
+    assert isinstance(obligatory_edit_section.children[0], ssb.Title)
     assert isinstance(obligatory_edit_section.children[1], dbc.Form)
 
 
-# Why function with empty list?
-def test_build_input_field_section():
+@pytest.mark.parametrize(
+    ("metadata_inputs", "variable", "language"),
+    INPUT_FIELDS,
+)
+def test_build_input_field_section(metadata_inputs, variable, language):
     test_input_section = build_input_field_section(
-        OBLIGATORY_VARIABLES_METADATA,
-        "pers_id",
-        NORSK_NYNORSK,
+        metadata_inputs,
+        variable,
+        language,
     )
-    assert test_input_section is not None
-    assert isinstance(test_input_section, dbc.Form)
     assert (
         isinstance(
             (test_input_section.children[i], (ssb.Input, ssb.Dropdown, dbc.Checkbox)),
         )
-        for i in test_input_section
+        for i in enumerate(INPUT_FIELDS)
     )
-    assert len(test_input_section.children) == len(OBLIGATORY_VARIABLES_METADATA)
 
 
-def test_input_section_will_build_form_but_no_components_with_empty_metadata_list():
+def test_build_input_field_section_no_input_return_empty_list():
     test_input_section = build_input_field_section(
         empty_metadata_input,
-        "pers_id",
-        NORSK_NYNORSK,
+        "",
+        "",
     )
-    assert isinstance(test_input_section, dbc.Form)
     assert test_input_section.children == []
