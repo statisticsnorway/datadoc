886cd5dc4b5c770f3d84b5477e628d726d90dc24
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-03-11 12:13:46 +0100

    Correct list

--- tests/frontend/components/test_build_input_section.py
+++ tests/frontend/components/test_build_input_section.py
@@ -10,6 +10,7 @@ from datadoc.enums import SupportedLanguages
 from datadoc.frontend.components.builders import build_input_field_section
 from datadoc.frontend.fields.display_base import get_enum_options_for_language
 from datadoc.frontend.fields.display_variables import OBLIGATORY_VARIABLES_METADATA
+from datadoc.frontend.fields.display_variables import OPTIONAL_VARIABLES_METADATA
 
 NORSK_BOKMÅL = "nb"
 NORSK_NYNORSK = "nn"
@@ -66,43 +67,6 @@ def test_build_input_fields_props_input(input_field_section):
 
 
 @pytest.mark.parametrize(
-    ("input_field_section"),
-    INPUT_FIELD_SECTION,
-)
-def test_build_input_fields_props_checkbox(input_field_section):
-    variable_checkbox_field = input_field_section.children[4]
-    assert isinstance(variable_checkbox_field, dbc.Checkbox)
-    assert variable_checkbox_field._type == "Checkbox"  # noqa: SLF001
-    assert variable_checkbox_field.value is None
-    variable_checkbox_field.value = True
-    assert variable_checkbox_field.value is True
-
-
-@pytest.mark.parametrize(
-    ("input_field_section"),
-    INPUT_FIELD_SECTION,
-)
-def test_build_input_fields_props_url(input_field_section):
-    variable_input_field_for_url = input_field_section.children[3]
-    assert isinstance(variable_input_field_for_url, ssb.Input)
-    assert variable_input_field_for_url.type == "url"
-    assert variable_input_field_for_url.value is None
-    input_value = "https://hattemaker.com"
-    variable_input_field_for_url.value = input_value
-    assert variable_input_field_for_url.value == input_value
-
-
-@pytest.mark.parametrize(("input_field_section"), INPUT_FIELD_SECTION)
-def test_build_input_fields_props_optional_input_list(input_field_section):
-    variable_input_field_for_data_source = input_field_section.children[0]
-    assert variable_input_field_for_data_source.type == "text"
-    assert variable_input_field_for_data_source.value is None
-    assert isinstance(variable_input_field_for_data_source, ssb.Input)
-    assert variable_input_field_for_data_source.debounce is True
-    assert variable_input_field_for_data_source.label == "Datakilde"
-
-
-@pytest.mark.parametrize(
     ("input_field_section", "language"),
     [
         (
@@ -151,3 +115,64 @@ def test_build_dropdown_fields_props_dropdown(input_field_section, language):
         enums.VariableRole,
         SupportedLanguages(language),
     )
+
+
+@pytest.mark.parametrize(
+    ("input_field_section"),
+    INPUT_FIELD_SECTION,
+)
+def test_build_input_fields_props_checkbox(input_field_section):
+    variable_checkbox_field = input_field_section.children[4]
+    assert isinstance(variable_checkbox_field, dbc.Checkbox)
+    assert variable_checkbox_field._type == "Checkbox"  # noqa: SLF001
+    assert variable_checkbox_field.value is None
+    variable_checkbox_field.value = True
+    assert variable_checkbox_field.value is True
+
+
+@pytest.mark.parametrize(
+    ("input_field_section"),
+    INPUT_FIELD_SECTION,
+)
+def test_build_input_fields_props_url(input_field_section):
+    variable_input_field_for_url = input_field_section.children[3]
+    assert isinstance(variable_input_field_for_url, ssb.Input)
+    assert variable_input_field_for_url.type == "url"
+    assert variable_input_field_for_url.value is None
+    input_value = "https://hattemaker.com"
+    variable_input_field_for_url.value = input_value
+    assert variable_input_field_for_url.value == input_value
+
+
+@pytest.mark.parametrize(
+    ("input_field_section"),
+    [
+        build_input_field_section(
+            OPTIONAL_VARIABLES_METADATA,
+            model.Variable(short_name="hoveddiagnose"),
+            NORSK_NYNORSK,
+        ),
+        build_input_field_section(
+            OPTIONAL_VARIABLES_METADATA,
+            model.Variable(short_name="pers_id"),
+            NORSK_BOKMÅL,
+        ),
+        build_input_field_section(
+            OPTIONAL_VARIABLES_METADATA,
+            model.Variable(short_name="ber_bruttoformue"),
+            ENGLISH,
+        ),
+        build_input_field_section(
+            OPTIONAL_VARIABLES_METADATA,
+            model.Variable(short_name="sykepenger"),
+            NORSK_BOKMÅL,
+        ),
+    ],
+)
+def test_build_input_fields_props_optional_input_list(input_field_section):
+    variable_input_field_for_data_source = input_field_section.children[0]
+    assert variable_input_field_for_data_source.type == "text"
+    assert variable_input_field_for_data_source.value is None
+    assert isinstance(variable_input_field_for_data_source, ssb.Input)
+    assert variable_input_field_for_data_source.debounce is True
+    assert variable_input_field_for_data_source.label == "Datakilde"
