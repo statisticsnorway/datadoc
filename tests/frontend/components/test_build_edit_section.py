86415ddd35f39f9c9c386bedff0eb377caa2af8b
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-03-17 12:21:35 +0100

    tests edit_section

--- tests/frontend/components/test_build_edit_section.py
+++ tests/frontend/components/test_build_edit_section.py
@@ -1,6 +1,8 @@
 """Test function build_edit_section."""
 
+import dash_bootstrap_components as dbc
 import pytest
+import ssb_dash_components as ssb  # type: ignore[import-untyped]
 from dash import html
 from datadoc_model import model
 
@@ -21,9 +23,48 @@ from datadoc.frontend.fields.display_variables import OBLIGATORY_VARIABLES_METAD
         ),
     ],
 )
-def test_build_edit_section(build_edit_section):
+def test_build_edit_section_return_correct_component(build_edit_section):
+    """Assert method returns html section."""
     assert isinstance(build_edit_section, html.Section)
+
+
+@pytest.mark.parametrize(
+    ("build_edit_section"),
+    [
+        build_edit_section([], "", model.Variable(), ""),
+        build_edit_section(
+            OBLIGATORY_VARIABLES_METADATA,
+            "",
+            model.Variable(short_name="sykepenger"),
+            SupportedLanguages.NORSK_NYNORSK,
+        ),
+    ],
+)
+def test_build_edit_section_children_return_correct_components(build_edit_section):
+    """Assert method has a list of children which returns SSB dash component Title and dash bootstrap Form."""
     title = build_edit_section.children[0].children
     form = build_edit_section.children[1]
-    assert title == build_edit_section.id["title"]
-    assert form is not None
+    assert isinstance(title, ssb.Title)
+    assert isinstance(form, dbc.Form)
+
+
+@pytest.mark.parametrize(
+    ("build_edit_section"),
+    [
+        build_edit_section([], "", model.Variable(), ""),
+        build_edit_section(
+            OBLIGATORY_VARIABLES_METADATA,
+            "",
+            model.Variable(short_name="sykepenger"),
+            SupportedLanguages.NORSK_NYNORSK,
+        ),
+    ],
+)
+def test_build_edit_section_has_correct_id(build_edit_section):
+    """Assert dictionary id is build correctly with 'type' and 'title'.
+
+    'title' is children of Title component (which is the same as the content/value of Title)
+    """
+    title = build_edit_section.children[0].children
+    edit_section_id = {"type": "edit-section", "title": title}
+    assert edit_section_id == build_edit_section.id
