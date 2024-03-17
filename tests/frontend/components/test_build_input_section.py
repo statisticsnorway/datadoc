f80b7ffc6a8a0fc0b2248419b70ebd31c7537b18
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-03-17 16:08:30 +0100

    check between component and class

--- tests/frontend/components/test_build_input_section.py
+++ tests/frontend/components/test_build_input_section.py
@@ -5,14 +5,12 @@ import pytest
 import ssb_dash_components as ssb  # type: ignore[import-untyped]
 from datadoc_model import model
 
-from datadoc import enums
 from datadoc.enums import SupportedLanguages
 from datadoc.frontend.components.builders import build_input_field_section
 from datadoc.frontend.fields.display_base import VariablesCheckboxField
 from datadoc.frontend.fields.display_base import VariablesDropdownField
 from datadoc.frontend.fields.display_base import VariablesInputField
 from datadoc.frontend.fields.display_base import VariablesPeriodField
-from datadoc.frontend.fields.display_base import get_enum_options_for_language
 from datadoc.frontend.fields.display_variables import OBLIGATORY_VARIABLES_METADATA
 from datadoc.frontend.fields.display_variables import OPTIONAL_VARIABLES_METADATA
 
@@ -72,6 +70,10 @@ def test_build_input_fields_input_components(input_field_section):
         type(elements_of_input[i]) == VariablesInputField
         for i, field in enumerate(elements_of_input)
     ]
+    assert [
+        elements_of_input[i].debounce is True
+        for i, field in enumerate(elements_of_input)
+    ]
 
 
 @pytest.mark.parametrize(
@@ -84,17 +86,18 @@ def test_build_input_fields_dropdown_components(input_field_section):
     elements_of_dropdown = list(
         filter(lambda x: isinstance(x, desired_type), input_field_section.children),
     )
-    assert (
-        isinstance(elements_of_dropdown[i], desired_type) for i in elements_of_dropdown
-    )
-    assert (
+    assert [
+        isinstance(elements_of_dropdown[i], desired_type)
+        for i, field in enumerate(elements_of_dropdown)
+    ]
+    assert [
         isinstance(elements_of_dropdown[i], VariablesDropdownField)
-        for i in elements_of_dropdown
-    )
-    assert (
+        for i, field in enumerate(elements_of_dropdown)
+    ]
+    assert [
         elements_of_dropdown[i]._type == "Dropdown"  # noqa: SLF001
-        for i in elements_of_dropdown
-    )
+        for i, field in enumerate(elements_of_dropdown)
+    ]
 
 
 @pytest.mark.parametrize(
@@ -107,20 +110,22 @@ def test_build_input_fields_checkbox_components(input_field_section):
     elements_of_checkbox = list(
         filter(lambda x: isinstance(x, desired_type), input_field_section.children),
     )
-    assert (
-        isinstance(elements_of_checkbox[i], desired_type) for i in elements_of_checkbox
-    )
-    assert (
+    assert [
+        isinstance(elements_of_checkbox[i], desired_type)
+        for i, field in enumerate(elements_of_checkbox)
+    ]
+    assert [
         isinstance(elements_of_checkbox[i], VariablesCheckboxField)
-        for i in elements_of_checkbox
-    )
-    assert (
+        for i, field in enumerate(elements_of_checkbox)
+    ]
+    assert [
         elements_of_checkbox[i]._type == "Checkbox"  # noqa: SLF001
-        for i in elements_of_checkbox
-    )
-    assert (
-        elements_of_checkbox[i].description is not None for i in elements_of_checkbox
-    )
+        for i, field in enumerate(elements_of_checkbox)
+    ]
+    assert [
+        elements_of_checkbox[i].disabled is False
+        for i, field in enumerate(elements_of_checkbox)
+    ]
 
 
 @pytest.mark.parametrize(
@@ -151,9 +156,10 @@ def test_build_input_fields_type_date(input_field_section):
     elements_of_date = list(
         filter(lambda x: (x.type == "date"), elements_of_input),
     )
-    assert (
-        isinstance(elements_of_date[i], VariablesPeriodField) for i in elements_of_date
-    )
+    assert [
+        isinstance(elements_of_date[i], VariablesPeriodField)
+        for i, field in enumerate(elements_of_date)
+    ]
 
 
 @pytest.mark.parametrize(
@@ -199,37 +205,57 @@ def test_build_input_fields_props(input_field_section, language):
     elements_of_input = list(
         filter(lambda x: isinstance(x, type_input), input_field_section.children),
     )
+    variable_identifier_input = list(
+        filter(lambda x: isinstance(x, VariablesInputField), VARIABLES_METADATA),
+    )
+    variable_identifier_input_date = list(
+        filter(lambda x: isinstance(x, VariablesPeriodField), VARIABLES_METADATA),
+    )
+    variable_inputs = variable_identifier_input + variable_identifier_input_date
     type_dropdown = ssb.Dropdown
     elements_of_dropdown = list(
         filter(lambda x: isinstance(x, type_dropdown), input_field_section.children),
+    )
+    variable_identifier_dropdown = list(
+        filter(lambda x: isinstance(x, VariablesDropdownField), VARIABLES_METADATA),
     )
     type_checkbox = dbc.Checkbox
     elements_of_checkbox = list(
         filter(lambda x: isinstance(x, type_checkbox), input_field_section.children),
     )
+    variable_identifier_checkbox = list(
+        filter(lambda x: isinstance(x, VariablesCheckboxField), VARIABLES_METADATA),
+    )
     elements_of_date = list(
         filter(lambda x: (x.type == "date"), elements_of_input),
     )
-    assert (elements_of_input[i].debounce is True for i in elements_of_input)
-    assert (elements_of_checkbox[i].disabled is False for i in elements_of_checkbox)
-    assert (
-        elements_of_dropdown[i].items
-        == get_enum_options_for_language(
-            enums[i].__name__,
-            SupportedLanguages(language),
+    assert [
+        field.items == variable_identifier_dropdown[i].options_getter(language)
+        for i, field in enumerate(
+            elements_of_dropdown,
         )
-        for i in elements_of_dropdown
-    )
-    assert (
-        elements_of_input[i].label == elements_of_input[i].display_name
-        for i in elements_of_input
-    )
-    assert (
-        elements_of_dropdown[i].header == elements_of_dropdown[i].display_name
-        for i in elements_of_dropdown
-    )
-    assert (
-        elements_of_checkbox[i].label == elements_of_checkbox[i].display_name
-        for i in elements_of_checkbox
-    )
-    assert (elements_of_date[i].debounce is False for i in elements_of_checkbox)
+    ]
+    assert [
+        elements_of_input[i].label == variable_inputs[i].display_name
+        for i, field in enumerate(
+            elements_of_input,
+        )
+    ]
+    assert [
+        elements_of_dropdown[i].header == variable_identifier_dropdown[i].display_name
+        for i, field in enumerate(
+            elements_of_dropdown,
+        )
+    ]
+    assert [
+        elements_of_checkbox[i].label == variable_identifier_checkbox[i].display_name
+        for i, field in enumerate(
+            elements_of_checkbox,
+        )
+    ]
+    assert [
+        elements_of_date[i].debounce is False
+        for i, field in enumerate(
+            variable_identifier_input_date,
+        )
+    ]
