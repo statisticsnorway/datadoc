6f9da0a8d58356cb8d2d482bf0569f1c3affbe06
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-03-24 14:04:03 +0100

    Update tests minus atypical options-getters

--- tests/frontend/components/test_build_dataset_tab.py
+++ tests/frontend/components/test_build_dataset_tab.py
@@ -1,24 +1,29 @@
 """Test function build_dataset_edit_section."""
 
-import functools
-
 import dash_bootstrap_components as dbc
 import pytest
 import ssb_dash_components as ssb  # type: ignore[import-untyped]
 from dash import html
 from datadoc_model import model
 
-from datadoc import enums
 from datadoc.enums import SupportedLanguages
 from datadoc.frontend.components.builders import build_dataset_edit_section
-from datadoc.frontend.fields.display_base import INPUT_KWARGS
-from datadoc.frontend.fields.display_base import DatasetDropdownField
-from datadoc.frontend.fields.display_base import DatasetInputField
-from datadoc.frontend.fields.display_base import get_enum_options_for_language
-from datadoc.frontend.fields.display_base import get_metadata_and_stringify
+from datadoc.frontend.fields.display_dataset import DISPLAY_DATASET
 from datadoc.frontend.fields.display_dataset import NON_EDITABLE_DATASET_METADATA
 from datadoc.frontend.fields.display_dataset import OPTIONAL_DATASET_METADATA
 from datadoc.frontend.fields.display_dataset import DatasetIdentifiers
+
+# List of obligatory and editable fields minus dropdowns with atypical options-getters
+OBLIGATORY_MINUS_ATYPICAL_DROPDOWNS = [
+    m
+    for m in DISPLAY_DATASET.values()
+    if m.obligatory
+    and m.editable
+    and m.identifier != DatasetIdentifiers.UNIT_TYPE.value
+    and m.identifier != DatasetIdentifiers.KEYWORD.value
+    and m.identifier != DatasetIdentifiers.SUBJECT_FIELD.value
+    and m.identifier != DatasetIdentifiers.OWNER.value
+]
 
 INPUT_DATA = [
     (
@@ -34,27 +39,6 @@ INPUT_DATA = [
         SupportedLanguages.NORSK_BOKMÅL,
         model.Dataset(short_name="fantastic_dataset"),
         {"type": "dataset-edit-section", "id": "obligatory-nb"},
-    ),
-    (
-        "",
-        [],
-        "",
-        model.Dataset(short_name="amazing_dataset"),
-        {"type": "dataset-edit-section", "id": "obligatory-nb"},
-    ),
-    (
-        "",
-        [],
-        "",
-        model.Dataset(short_name="unbelievable_dataset"),
-        {},
-    ),
-    (
-        "",
-        [],
-        "",
-        model.Dataset(),
-        {},
     ),
     (
         "",
@@ -86,88 +70,7 @@ INPUT_DATA = [
     ),
     (
         "Lazy title",
-        [
-            DatasetInputField(
-                identifier=DatasetIdentifiers.SHORT_NAME.value,
-                display_name="Kortnavn",
-                description="Navn på (fysisk) datafil, datatabell eller datasett",
-                obligatory=True,
-                editable=False,
-            ),
-            DatasetDropdownField(
-                identifier=DatasetIdentifiers.DATASET_STATUS.value,
-                display_name="Status",
-                description="Livssyklus for datasettet",
-                options_getter=functools.partial(
-                    get_enum_options_for_language,
-                    enums.DataSetStatus,
-                ),
-                obligatory=True,
-            ),
-            DatasetDropdownField(
-                identifier=DatasetIdentifiers.DATASET_STATE.value,
-                display_name="Datatilstand",
-                description="Datatilstand. Se Intern dokument 2021- 17  Datatilstander i SSB",
-                obligatory=True,
-                options_getter=functools.partial(
-                    get_enum_options_for_language,
-                    enums.DataSetState,
-                ),
-            ),
-            DatasetInputField(
-                identifier=DatasetIdentifiers.NAME.value,
-                display_name="Navn",
-                description="Datasettnavn",
-                obligatory=True,
-                multiple_language_support=True,
-                value_getter=get_metadata_and_stringify,
-            ),
-            DatasetInputField(
-                identifier=DatasetIdentifiers.DATA_SOURCE.value,
-                display_name="Datakilde",
-                description="Datakilde. Settes enten for datasettet eller variabelforekomst.",
-                obligatory=True,
-                multiple_language_support=True,
-            ),
-            DatasetInputField(
-                identifier=DatasetIdentifiers.POPULATION_DESCRIPTION.value,
-                display_name="Populasjon",
-                description="Populasjonen datasettet dekker. Populasjonsbeskrivelsen inkluderer enhetstype, geografisk dekningsområde og tidsperiode.",
-                obligatory=True,
-                multiple_language_support=True,
-            ),
-            DatasetInputField(
-                identifier=DatasetIdentifiers.VERSION.value,
-                display_name="Versjon",
-                description="Versjon",
-                extra_kwargs=dict(type="number", min=1, **INPUT_KWARGS),
-                obligatory=True,
-            ),
-            DatasetInputField(
-                identifier=DatasetIdentifiers.VERSION_DESCRIPTION.value,
-                display_name="Versjonsbeskrivelse",
-                description="Årsak/grunnlag for denne versjonen av datasettet i form av beskrivende tekst.",
-                multiple_language_support=True,
-                obligatory=True,
-            ),
-            DatasetDropdownField(
-                identifier=DatasetIdentifiers.TEMPORALITY_TYPE.value,
-                display_name="Temporalitetstype",
-                description="Temporalitetstype. Settes enten for variabelforekomst eller datasett. Se Temporalitet, hendelser og forløp.",
-                options_getter=functools.partial(
-                    get_enum_options_for_language,
-                    enums.TemporalityTypeType,
-                ),
-                obligatory=True,
-            ),
-            DatasetInputField(
-                identifier=DatasetIdentifiers.DESCRIPTION.value,
-                display_name="Beskrivelse",
-                description="Beskrivelse av datasettet",
-                multiple_language_support=True,
-                obligatory=True,
-            ),
-        ],
+        OBLIGATORY_MINUS_ATYPICAL_DROPDOWNS,
         SupportedLanguages.NORSK_BOKMÅL,
         model.Dataset(short_name="nosy_dataset"),
         {"type": "dataset-edit-section", "id": "obligatory-nb"},
@@ -228,33 +131,62 @@ def test_build_dataset_form(title, field_types, language, dataset, key):
 
 
 @pytest.mark.parametrize(
-    ("title", "field_types", "language", "dataset", "key"),
-    INPUT_DATA,
+    ("edit_section", "expected_inputs", "expected_dropdowns"),
+    [
+        (
+            build_dataset_edit_section(
+                "",
+                NON_EDITABLE_DATASET_METADATA,
+                SupportedLanguages.NORSK_BOKMÅL,
+                model.Dataset(short_name="fantastic_dataset"),
+                {"type": "dataset-edit-section", "id": "obligatory-nb"},
+            ),
+            7,
+            0,
+        ),
+        (
+            build_dataset_edit_section(
+                "New title",
+                OPTIONAL_DATASET_METADATA,
+                SupportedLanguages.NORSK_NYNORSK,
+                model.Dataset(),
+                {},
+            ),
+            3,
+            1,
+        ),
+        (
+            build_dataset_edit_section(
+                "",
+                OBLIGATORY_MINUS_ATYPICAL_DROPDOWNS,
+                SupportedLanguages.ENGLISH,
+                model.Dataset(short_name="super_dataset"),
+                {"type": "dataset-edit-section", "id": "obligatory-en"},
+            ),
+            8,
+            3,
+        ),
+    ],
 )
-def test_build_dataset_input_fields(title, field_types, language, dataset, key):
-    edit_section = build_dataset_edit_section(
-        title,
-        field_types,
-        language,
-        dataset,
-        key,
-    )
+def test_build_dataset_input_fields_from_datasetidentifiers_class(
+    edit_section,
+    expected_inputs,
+    expected_dropdowns,
+):
     dataset_form = edit_section.children[1]
-    inputs = [element for element in dataset_form if isinstance(element, ssb.Input)]
-    dropdowns = [
-        element for element in dataset_form if isinstance(element, ssb.Dropdown)
-    ]
-    dataset = model.Dataset()
-    dataset_identifier_input = [
-        element
-        for element in OPTIONAL_DATASET_METADATA
-        if isinstance(element, DatasetInputField)
-    ]
-    assert dataset_identifier_input != []
-    assert dataset.unit_type is None
+    fields = dataset_form.children
+    inputs = [element for element in fields if isinstance(element, ssb.Input)]
+    dropdowns = [element for element in fields if isinstance(element, ssb.Dropdown)]
     for item in inputs:
-        assert isinstance(item, ssb.Input)
+        if item.type != "date":
+            assert item.debounce is True
+    for item in inputs:
+        assert item.type in ("text", "url", "date")
+    for item in inputs:
+        assert item.label != ""
     for item in dropdowns:
-        assert isinstance(item, ssb.Dropdown)
-    for item in inputs:
-        assert item.debounce is True
+        assert item.items is not None
+    for item in dropdowns:
+        assert item.header != ""
+    assert len(inputs) == expected_inputs
+    assert len(dropdowns) == expected_dropdowns
