e9cd87600b97233a1f967f7fb1b74ec73fa232f5
Joergen <rlj@ssb.no>
2024-04-15 10:42:09 +0200

    Fixed pre commit errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -129,7 +129,9 @@ def nynorsk_name() -> str:
 
 @pytest.fixture()
 def language_object(
-    english_name: str, bokmål_name: str, nynorsk_name: str,
+    english_name: str,
+    bokmål_name: str,
+    nynorsk_name: str,
 ) -> model.LanguageStringType:
     return model.LanguageStringType(
         [
