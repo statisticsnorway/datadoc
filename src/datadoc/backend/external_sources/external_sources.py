608424cf919f147da3fb5d44bb79a7a8776c2ad1
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-07-23 11:01:17 +0200

    docstrings external sources

--- src/datadoc/backend/external_sources/external_sources.py
+++ src/datadoc/backend/external_sources/external_sources.py
@@ -16,20 +16,19 @@ T = TypeVar("T")
 
 
 class GetExternalSource(ABC, Generic[T]):
-    """Abstract base class for getting data from external sources.
+    """Abstract base class for retrieving data from external sources asynchronously.
 
     This class provides methods to initiate an asynchronous data retrieval
     operation, check its status, and retrieve the result once the operation
     completes. Subclasses must implement the `_fetch_data_from_external_source`
     method to define how data is fetched from the specific external source.
-
     """
 
     def __init__(self, executor: concurrent.futures.ThreadPoolExecutor) -> None:
-        """Initializes the GetExternalSource with an executor to manage asynchronous tasks.
+        """Initialize the GetExternalSource with an executor to manage asynchronous tasks.
 
-        Initializes the future object that will hold the result of the asynchronous
-        data fetching operation from external source.
+        This constructor initializes a future object that will hold the result of the
+        asynchronous data fetching operation from an external source.
 
         Args:
             executor: An instance of ThreadPoolExecutor to manage the asynchronous execution of data fetching.
@@ -39,9 +38,9 @@ class GetExternalSource(ABC, Generic[T]):
         )
 
     def wait_for_external_result(self) -> None:
-        """Waits for the thread responsible for loading the external request to finish.
+        """Wait for the thread responsible for loading the external request to finish.
 
-        If there is no future to wait for, logs a warning and returns immediately.
+        If there is no future to wait for, it logs a warning and returns immediately.
         """
         if not self.future:
             logger.warning("No future to wait for.")
@@ -49,7 +48,7 @@ class GetExternalSource(ABC, Generic[T]):
         self.future.result()
 
     def check_if_external_data_is_loaded(self) -> bool:
-        """Method to check if the thread getting the external data has finished running.
+        """Check if the thread getting the external data has finished running.
 
         Returns:
             bool: True if the data fetching operation is complete, False otherwise.
@@ -59,12 +58,14 @@ class GetExternalSource(ABC, Generic[T]):
         return False
 
     def retrieve_external_data(self) -> T | None:
-        """Method that returns the result of the thread.
+        """Retrieve the result of the data fetching operation.
+
+        This method checks if the asynchronous data fetching operation has completed.
+        If the operation is finished, it returns the result. Otherwise, it returns None.
 
         Returns:
-            The result of the data fetching operation if it is complete, or
-            None if the operation has not yet finished.
-
+            T | None: The result of the data fetching operation if it is complete,
+                or None if the operation has not yet finished.
         """
         if self.future:
             return self.future.result()
@@ -72,9 +73,11 @@ class GetExternalSource(ABC, Generic[T]):
 
     @abstractmethod
     def _fetch_data_from_external_source(self) -> T | None:
-        """Abstract method implemented in the child class to handle the external data.
+        """Abstract method to be implemented in the subclass to handle external data retrieval.
 
-        This method should define the logic for retrieving data from the
-        specific external source.
+        This method should define the logic for retrieving data from the specific external source.
+
+        Returns:
+            T | None: The data retrieved from the external source.
         """
         raise NotImplementedError
